9f35594f8ebe8e337c0bb70677987ebbce378de8
RichardTorresRivera <rmtr2711@gmail.com>
2025-05-04 11:13:15 -0500

    add @tanstack/react-query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,23 @@ import { AuthProvider } from "./context/AuthContext";
 import Rutas from "./routes/index.routes";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <AuthProvider>
-          <Rutas />
-        </AuthProvider>
-      </BrowserRouter>
-      <ToastContainer />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <Rutas />
+          </AuthProvider>
+        </BrowserRouter>
+        <ToastContainer />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
